a77ba391fcec94651c0179e718e6c06d4069b079
kstutz <kathi.stutz@1und1.de>
2012-09-17 16:54:14 +0200

    Added form validation and icon

--- src/main/java/com/unitedinternet/jenkins/plugins/jobtemplates/JobTemplates.java
+++ src/main/java/com/unitedinternet/jenkins/plugins/jobtemplates/JobTemplates.java
@@ -2,20 +2,26 @@ package com.unitedinternet.jenkins.plugins.jobtemplates;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.servlet.ServletException;
+
+import jenkins.model.Jenkins;
+
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
 import hudson.Extension;
 import hudson.model.Hudson;
+import hudson.model.Failure;
 import hudson.model.Item;
 import hudson.model.RootAction;
 import hudson.model.TopLevelItem;
-
-
+import hudson.util.FormValidation;
 
 
 /**
@@ -25,26 +31,16 @@ import hudson.model.TopLevelItem;
 @Extension
 public class JobTemplates implements RootAction {
 
-    /**Meet our logger.*/
-    private static final Logger LOGGER = Logger.getLogger(JobTemplates.class.getName());
+    private static final Logger LOG = Logger.getLogger(JobTemplates.class.getName());
 
-    /**
-     * {@inheritDoc}
-     */
     public final String getIconFileName() {
         return "/plugin/jobtemplates/icons/jobtemplates-32x32.png";
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public final String getDisplayName() {
-        return "JobTemplates";
+        return "New Job from Template";
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public final String getUrlName() {
         return "/jobtemplates";
     }
@@ -52,7 +48,6 @@ public class JobTemplates implements RootAction {
     public ArrayList<Item> getTemplates() {
         final ArrayList<Item> templateList = new ArrayList<Item>(); 
         
-        //ersetzen durch getAllJobNames (Collection!)
         final List<Item> getitems = Hudson.getInstance().getAllItems(Item.class);
         for (Item item : getitems) {
             if (item.getName().startsWith("Template_")) {
@@ -69,9 +64,36 @@ public class JobTemplates implements RootAction {
         final String newName = req.getParameter("newname");
         final String templateName = req.getParameter("template");
         
+        try {
+            Jenkins.checkGoodName(newName);
+        } catch (Failure f) {
+            writeMessage(req, rsp, f.getMessage());
+            return;
+        }
+
+        if (jenkins.getItem(newName) != null){
+            writeMessage(req, rsp, "A job with this name already exists.");
+            return;
+        }
+        
+        if (templateName == null){
+            writeMessage(req, rsp, "Please choose a job to copy.");
+            return;
+        }
+        
         final Item template = jenkins.getItem(templateName);
         final Item newJob = Hudson.getInstance().copy((TopLevelItem)template, newName);
-        
         rsp.sendRedirect(jenkins.getRootUrl() + newJob.getUrl() + "configure");
+    }
+    
+    private void writeMessage(StaplerRequest req, StaplerResponse rsp, String s)
+            throws IOException{
+        
+        final Writer writer = rsp.getCompressedWriter(req);
+        try {
+            writer.append(s);
+        } finally {
+            writer.close();
+        }
     }
 }
