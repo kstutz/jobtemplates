64c8eeff1b61f402bc26b8b3e7841b58ae8f9f9e
kstutz <kathi.stutz@1und1.de>
2013-09-25 15:40:38 +0200

    Checkstyle.

--- src/main/java/com/unitedinternet/jenkins/plugins/jobtemplates/JobTemplates.java
+++ src/main/java/com/unitedinternet/jenkins/plugins/jobtemplates/JobTemplates.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import jenkins.model.Jenkins;
 
@@ -19,8 +18,6 @@ import hudson.model.Failure;
 import hudson.model.Item;
 import hudson.model.RootAction;
 import hudson.model.TopLevelItem;
-import hudson.security.Permission;
-
 
 /**
  *
@@ -28,8 +25,6 @@ import hudson.security.Permission;
  */
 @Extension
 public class JobTemplates implements RootAction {
-
-//    private static final Logger LOG = Logger.getLogger(JobTemplates.class.getName());
 
     /**
      * The hudson instance.
@@ -43,23 +38,41 @@ public class JobTemplates implements RootAction {
         jenkins = Hudson.getInstance();
     }
     
-    public final Hudson getJenkins(){
+    /**
+     * @return Jenkins instance
+     */
+    public final Hudson getJenkins() {
         return jenkins;
     }
     
+    /**
+     * Return the icon path if user has 'Create' permission.
+     * @return Path to the icon
+     */
     public final String getIconFileName() {
-        return jenkins.hasPermission(AbstractProject.CREATE) ? 
-            "/plugin/jobtemplates/icons/jobtemplates-32x32.png" : null;
+        return jenkins.hasPermission(AbstractProject.CREATE) 
+            ? "/plugin/jobtemplates/icons/jobtemplates-32x32.png" : null;
     }
 
+    /**
+     * @return Display name as String.
+     */
     public final String getDisplayName() {
         return Messages.displayName();
     }
 
+    /**
+     * @return URL name as String.
+     */
     public final String getUrlName() {
         return "/jobtemplates";
     }
 
+    /**
+     * Returns the available job templates.
+     * (i.e. all jobs with names starting with 'template_')
+     * @return ArrayList of template jobs
+     */
     public ArrayList<Item> getTemplates() {
         final ArrayList<Item> templateList = new ArrayList<Item>(); 
         
@@ -72,12 +85,18 @@ public class JobTemplates implements RootAction {
         return templateList;
     }
     
+    /**
+     * Creates a new job by copying the config of the template job.
+     * @param req The StaplerRequest
+     * @param rsp TheStaplerResponse
+     * @throws IOException Thrown when job creation goes wrong
+     */
     public final void doCreateJob(StaplerRequest req, StaplerResponse rsp)
-            throws IOException {
+        throws IOException {
 
         try {
             jenkins.checkPermission(AbstractProject.CREATE);
-        } catch (AccessDeniedException ex){
+        } catch (AccessDeniedException ex) {
             writeMessage(req, rsp, ex.getMessage());
             return;
         }
@@ -92,23 +111,30 @@ public class JobTemplates implements RootAction {
             return;
         }
 
-        if (jenkins.getItem(newName) != null){
+        if (jenkins.getItem(newName) != null) {
             writeMessage(req, rsp, Messages.jobExists());
             return;
         }
         
-        if (templateName == null){
+        if (templateName == null) {
             writeMessage(req, rsp, Messages.chooseJob());
             return;
         }
         
         final Item template = jenkins.getItem(templateName);
-        final Item newJob = jenkins.copy((TopLevelItem)template, newName);
+        final Item newJob = jenkins.copy((TopLevelItem) template, newName);
         rsp.sendRedirect(jenkins.getRootUrl() + newJob.getUrl() + "configure");
     }
     
+    /**
+     * Writes an error message.
+     * @param req StaplerRequest
+     * @param rsp StaplerResponse
+     * @param s Message string
+     * @throws IOException Thrown when writing goes wrong
+     */
     private void writeMessage(StaplerRequest req, StaplerResponse rsp, String s)
-            throws IOException{
+        throws IOException {
         
         final Writer writer = rsp.getCompressedWriter(req);
         try {
